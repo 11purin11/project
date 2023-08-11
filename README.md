228566b19f490c96e05c531fa22a5da331f91f8f
11purin11 <autosen195@gmail.com>
2023-08-11 18:30:19 +0900

    first update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # project
 2023/08/11 公開サイト　https://4c6b5507.project-28o.pages.dev
 （CloudFlare Pagesの利用）
+最終更新：2023/08/11 18:28
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 const fileInput = document.getElementById("fileInput");
 const fileTable = document.getElementById("fileTable").getElementsByTagName("tbody")[0];
 const addButton = document.getElementById('addButton');
@@ -7,7 +8,9 @@ const loopButton = document.getElementById('loopButton');
 
 const audioPlayer = document.getElementById("audioPlayer");
 const audioSource = document.getElementById("audioSource");
+audioPlayer.volume=0.5;
 
+const sliderName = document.getElementById("sliderName");
 const slider = document.getElementById("sliderContainer");
 const playnow = document.getElementById("playnow");
 
@@ -18,6 +21,37 @@ let currentmusic=0;
 let startlist = [];
 let endlist = [];
 let loop = false;
+
+noUiSlider.create(slider, {
+  start: [0, 120],                   //スライダーバーの初期位置を指定
+  connect:true,
+  step: 1,
+  range: {
+    'min': 0,                       //スライダーバーの最小値
+    'max': 120   //スライダーバーの最大値
+  },
+  tooltips: true,
+  pips: {
+    mode: 'values',
+    values: [60],
+    format: {
+      to: function(value){
+        return customFormat(value);
+      },
+      from: function(value){
+        return value;
+      }
+    }
+  },
+  format: {
+    to: function(value){
+      return convertSecToMin(value);
+    },
+    from: function(value){
+      return value;
+    }
+  }
+});
 
 addButton.addEventListener('click', () => {
   const file = fileInput.files[0];
@@ -36,12 +70,12 @@ addButton.addEventListener('click', () => {
         startlist[startlist.length]=0.0;
         endlist[endlist.length]=audio.duration;
 
-        nameCell.innerHTML = fileName;
+        nameCell.innerHTML = fileName+"("+convertSecToMin(audio.duration)+") ";
         Time.innerHTML = showTime(startlist.length-1);
         action.innerHTML = '<button onclick="customButton(this)">カスタム</button> <button onclick="deleteButton(this)">削除</button>';
 
         playlist[playlist.length]=audio;
-        namelist[namelist.length]=fileName;
+        namelist[namelist.length]=fileName+"("+convertSecToMin(audio.duration)+") ";
         if(playlist.length==1){
           audioSource.src = audio.src;
           audioPlayer.load();
@@ -49,6 +83,9 @@ addButton.addEventListener('click', () => {
     };
     reader.readAsText(file);
     fileInput.value="";
+    if(audioSource.src===undefined){
+      audioSource.src=audio.src;
+    }
   }else {
     alert('ファイルを選択してください。');
   }
@@ -71,6 +108,15 @@ function convertSecToMin(second){
 function showTime(index){
   return convertSecToMin(startlist[index])+" - " + convertSecToMin(endlist[index]);
 }
+function customFormat(value){
+  if(value===0){
+    return 'Start';
+  }else if(playlist.length!=0 && value===playlist[currentmusic].duration){
+    return 'End';
+  }else{
+    return convertSecToMin(value);
+  }
+}
 
 nextButton.addEventListener('click', () => {
   setMusic(currentmusic+1);
@@ -87,28 +133,59 @@ loopButton.addEventListener('click', () => {
   }
 });
 
+function setValues(range){
+  let result = [];
+  for(let i=30;i<range;i+=30){
+    result[result.length]=i;
+  }
+  return result;
+}
+
+function setSlider(index){
+  sliderName.style.display = "block";
+  slider.style.display = "block";
+  sliderName.innerHTML = "再生区間の調整曲： " + namelist[index];
+  let st=startlist[index];
+  let end=endlist[index];
+  let range = setValues(playlist[index].duration);
+  slider.noUiSlider.updateOptions({
+    start:[st,end],
+    range:{
+      'min': 0.0,
+      'max': playlist[index].duration
+    },
+    pips: {
+      mode: 'values',
+      values: range,
+      format: {
+        to: function(value){
+          return customFormat(value);
+        },
+        from: function(value){
+          return value;
+        }
+      }
+    },
+});
+}
+
+function offSlider(row,index){
+  let objSlide1 = $('.noUi-handle')[0];
+  let objSlide2 = $('.noUi-handle')[1];
+  startlist[index] = objSlide1.getAttribute('aria-valuenow');
+  endlist[index] = objSlide2.getAttribute('aria-valuenow');
+  row.cells[1].innerHTML = showTime(index);
+}
+
 function customButton(button){
   const row = button.parentNode.parentNode;
   const fileName = row.cells[0].textContent;
   const index = namelist.indexOf(fileName);
   if (slider.style.display === "none") {
-    slider.style.display = "block";
-    let st=startlist[index];
-    let end=endlist[index];
-    noUiSlider.create(slider, {
-      start: [st, end],                   //スライダーバーの初期位置を指定
-      range: {
-        'min': 0.0,                       //スライダーバーの最小値
-        'max': playlist[index].duration   //スライダーバーの最大値
-      }
-    });
-  } else {
-    let objSlide1 = $('.noUi-handle')[0];
-    let objSlide2 = $('.noUi-handle')[1];
-    startlist[index] = objSlide1.getAttribute('aria-valuenow');
-    endlist[index] = objSlide2.getAttribute('aria-valuenow');
-
-    row.cells[1].innerHTML = showTime(index);
+    setSlider(index);
+  }else {
+    offSlider(row,index);
+    sliderName.style.display = "none";
     slider.style.display = "none";
   }
 }
@@ -120,14 +197,17 @@ function deleteButton(button){
   const index = namelist.indexOf(fileName);
   if(playlist.length>1 && audioSource.src===playlist[index].src){
     setMusic(currentmusic+1);
-  }else{
+  }else if(playlist.length===1){
     audioSource.src=undefined;
+    audioPlayer.load();
   }
   playlist.splice(index,1);
   namelist.splice(index,1);
   startlist.splice(index,1);
   endlist.splice(index,1);
   table.removeChild(row);
+  sliderName.style.display = "none";
+  slider.style.display = "none";
 }
 
 function setMusic(index){
@@ -160,8 +240,9 @@ audioPlayer.addEventListener('timeupdate', () => {
 });
 
 audioPlayer.addEventListener('pause', () => {
-  playnow.innerHTML = "停止中";
-})
+  playnow.innerHTML = "";
+});
 audioPlayer.addEventListener('play', () => {
   playnow.innerHTML = "再生中：" + namelist[currentmusic];
-})
+});
+
